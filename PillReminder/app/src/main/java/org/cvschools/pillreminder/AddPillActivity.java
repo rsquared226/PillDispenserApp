2f48fb5e93930fe61452df4c5a78595bc7aee7b1
Rahul Ramkumar <rahulram226@gmail.com>
2016-12-30 14:41:20 -0500

    Fix AddPillFragment

--- PillReminder/app/src/main/java/org/cvschools/pillreminder/AddPillActivity.java
+++ PillReminder/app/src/main/java/org/cvschools/pillreminder/AddPillActivity.java
@@ -1,15 +1,46 @@
 package org.cvschools.pillreminder;
 
-import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-import org.cvschools.pillreminder.R;
+import java.util.Calendar;
 
-public class AddPillActivity extends Activity {
+public class AddPillActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_pill);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_pill);
+	}
+
+	public void datePick(View v) {
+		new DatePickerFragment().show(getSupportFragmentManager(), "datePicker");
+	}
+
+	public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+		@NonNull
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Use the current date as the default date in the picker
+			final Calendar c = Calendar.getInstance();
+			int year = c.get(Calendar.YEAR);
+			int month = c.get(Calendar.MONTH);
+			int day = c.get(Calendar.DAY_OF_MONTH);
+
+			// Create a new instance of DatePickerDialog and return it
+			return new DatePickerDialog(getActivity(), this, year, month, day);
+		}
+
+		@Override
+		public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+			
+		}
+	}
 }
--- PillReminder/app/src/main/java/org/cvschools/pillreminder/MainActivity.java
+++ PillReminder/app/src/main/java/org/cvschools/pillreminder/MainActivity.java
@@ -1,8 +1,10 @@
 package org.cvschools.pillreminder;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -13,13 +15,18 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+	private static final String LOG_TAG = "MainActivity";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
 
-	void fabPressed() {
-		startActivity(new Intent(MainActivity.this, AddPillActivity.this));
+		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+		fab.setOnClickListener(new FloatingActionButton.OnClickListener() {
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, AddPillActivity.class));
+			}
+		});
 	}
 }
